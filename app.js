e2cd78b411cb05bea9d20f095bc877195b345421
LipezJ <juandavidlipez47@gmail.com>
2021-09-05 12:44:59 -0500

    0.02

--- app.js
+++ app.js
@@ -5,34 +5,76 @@ var move2 = 0
 var event 
 var key
 var stilo 
-
 var Maxwi
+var Maxhe
+
+/* Funcion de movimientos */
 
 function move(event){
 
+    /* Obtencion de datos de la pantalla */
+
     key = event.keycode || event.which
+
+        /* Maximo de pantalla: width */
+    Maxwi = maxw * 5.5
+    Maxwi = Maxwi.toFixed(4)
+    Maxwi = parseFloat(Maxwi)
+
+    Maxhe = document.getElementById("cielo").style.height
+    Maxhe = parseFloat(Maxhe) - maxw
+
+        /* Maximo de pantalla: Heigth */
+    
+
+    /* Moviientos por teclas */
 
     if (key == 39) {
     
-        move1 = move1 + maxw 
-        stilo = move1.toString() + "px"
-        document.getElementById('player').style.marginLeft = stilo
-        
+        if (move1 < Maxwi) {
+
+            move1 = move1 + maxw
+            stilo = move1.toString() + "px"
+            document.getElementById('player').style.marginLeft = stilo
+
+        } 
+
     }
     if (key == 37) {
-        move1 = move1 - maxw  
-        stilo = move1.toString() + "px"
-        document.getElementById('player').style.marginLeft = stilo
+
+        if (move1 > 1) {
+
+            move1 = move1 - maxw 
+            stilo = move1.toString() + "px"
+            document.getElementById('player').style.marginLeft = stilo
+
+        } else {
+            document.getElementById('player').style.marginLeft = "0px"
         }
+
+    }
     if (key == 40) {
-        move2 = move2 + maxw  
-        stilo = move2.toString() + "px"
-        document.getElementById('player').style.marginTop = stilo
-        
+
+        if (move2 < Maxhe) {
+
+            move2 = move2 + maxw  
+            stilo = move2.toString() + "px"
+            document.getElementById('player').style.marginTop = stilo
+            
+        }
+
     }
     if (key == 38) {
-        move2 = move2 - maxw  
-        stilo = move2.toString() + "px"
-        document.getElementById('player').style.marginTop = stilo
+
+        if (move2 > 1) {
+
+            move2 = move2 - maxw  
+            stilo = move2.toString() + "px"
+            document.getElementById('player').style.marginTop = stilo
+
+        }else{
+            document.getElementById('player').style.marginTop = "0px"
+        }
+
     }
 }
--- stylos.js
+++ stylos.js
@@ -7,23 +7,108 @@ var tre
 var ert
 var ect
 var maxw 
-var maxw1 
+var maxw1
+var maxh 
+var maxh1
+
+/* Obtencion de datos de la pantalla */
 
 screen1 = window.screen.availHeight
 screen2 = window.screen.availWidth
 console.log(screen1, " ", screen2)
 
-cte = (screen1/100)*60
-tre = (screen1/100)*40
+/* Conversion de datos para usar en el codigo */
+
+    /* Divisiones de la pantalla */
+maxw = screen2 / 7 
+maxw1 = maxw.toString() + "px"
+
+    /* Division de cielo y tierra */
+
+if (screen2 >= 500) {
+    cte = maxw * 7
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd")
+}
+
+if (screen2 >= 300 && screen2 <= 500) {
+    cte = maxw * 8
+    tre = (screen1 - cte) 
+    cte = cte 
+    console.log("xd2")
+}
+
+if (screen1 >= 600) {
+
+    cte = maxw * 6
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd3")
+
+}
+
+if (screen2 >= 500) {
+
+    cte = maxw * 6
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd4")
+}
+
+if (screen2 < 300) {
+
+    cte = maxw * 13
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd5")
+
+}
+
+if (screen2 >= 360) {
+
+    cte = maxw * 8
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd6")
+
+}
+
+if (screen1 > 665 && screen2 < 400) {
+    cte = maxw * 9
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd7")
+}
+
+if (screen2 > 720 && screen1 > 1000) {
+
+    cte = maxw * 10
+    tre = (screen1 - cte) + (maxw * 2)
+    cte = cte - (maxw * 2)
+    console.log("xd8")
+
+}
+
+if (screen2 > 800) {
+    cte = maxw * 9
+    tre = (screen1 - cte) + (maxw * 3)
+    cte = cte - (maxw * 3)
+    console.log("xd9")
+}
+
+if (screen2 > 500 && screen1 > 700 && screen1 < 1000) {
+    cte = maxw * 7
+    tre = (screen1 - cte) + maxw
+    cte = cte - maxw
+    console.log("xd10")
+}
 
 cte = cte.toString() + "px"
 tre = tre.toString() + "px"
 
-etc = navigator.userAgent
-console.log(etc)
 
-maxw = screen2 / 7 
-maxw1 = maxw.toString() + "px"
+/* Funcion para cambiar los estilos segun los datos obtenidos */
 
 function sty(){
 
